edc2b88e9e2b2b89a461026586bb0c1a7496034c
Sabrina Silva <sabrina.silva@dbserver.com.br>
2022-10-20 13:55:14 -0300

    add teste para girar a esquerda

--- src/marsRover.spec.ts
+++ src/marsRover.spec.ts
@@ -37,3 +37,39 @@ describe('Testing giraParaDireita', () => {
     });
 
 });
+
+describe('Testing giraParaEsquerda', () => {
+
+    it('se direção for Norte deve mudar direção para Oeste', () => {
+        const coordenada = new Coordenada(0,0);
+        const marsRoverMock = new MarsRover(coordenada, Direcao.Norte) ;
+        const sut = marsRoverMock.giraParaEsquerda(marsRoverMock)
+    
+        expect(sut.direcao).toBe(Direcao.Oeste);
+    });
+
+    it('se direção for Leste deve mudar direção para Norte', () => {
+        const coordenada = new Coordenada(0,0);
+        const marsRoverMock = new MarsRover(coordenada, Direcao.Leste) ;
+        const sut = marsRoverMock.giraParaEsquerda(marsRoverMock)
+    
+        expect(sut.direcao).toBe(Direcao.Norte);
+    });
+  
+    it('se direção for Sul deve mudar direção para Leste', () => {
+        const coordenada = new Coordenada(0,0);
+        const marsRoverMock = new MarsRover(coordenada, Direcao.Sul) ;
+        const sut = marsRoverMock.giraParaEsquerda(marsRoverMock)
+    
+        expect(sut.direcao).toBe(Direcao.Leste);
+    });
+
+    it('se direção for Oeste deve mudar direção para Sul', () => {
+        const coordenada = new Coordenada(0,0);
+        const marsRoverMock = new MarsRover(coordenada, Direcao.Oeste) ;
+        const sut = marsRoverMock.giraParaEsquerda(marsRoverMock)
+    
+        expect(sut.direcao).toBe(Direcao.Sul);
+    });
+
+});
