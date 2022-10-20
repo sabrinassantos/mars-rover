d8e29c6d5ac378a67de8c2d305a055903c67d8d7
Sabrina Silva <sabrina.silva@dbserver.com.br>
2022-10-20 15:50:42 -0300

    refatoração classe coordenada: remoção de funções

--- src/coordenada.ts
+++ src/coordenada.ts
@@ -1,33 +1,9 @@
 export class Coordenada {
     constructor(
-        private x: number,
-        private y: number
+        public x: number,
+        public y: number
     ){
         this.x = x;
         this.y = y;
     }
-
-    moveParaNorte(coordenada: Coordenada) : Coordenada{       
-        coordenada.x = this.x;
-        coordenada.y = this.y + 1;
-        return coordenada;
-    }
-    moveParaSul(coordenada: Coordenada) : Coordenada{
-        coordenada.x = this.x;
-        coordenada.y = this.y - 1;
-        return coordenada;
-       
-    }
-    moveParaLeste(coordenada: Coordenada) : Coordenada{
-        coordenada.x = this.x + 1;
-        coordenada.y = this.y;
-        return coordenada;
-    }
-    moveParaOeste(coordenada: Coordenada): Coordenada{
-        coordenada.x = this.x - 1;
-        coordenada.y = this.y;
-        return coordenada;
-    }
-    
-    
 }
