183be47c3d39cd70d2f2db92596e52ed7440a87a
Sabrina Silva <sabrina.silva@dbserver.com.br>
2022-10-26 11:13:38 -0300

    teste comando

---
+++ src/app/index.spec.ts
@@ -0,0 +1,31 @@
+import comando from ".";
+import { Direcao } from "../direcao";
+import { Coordenada } from "../entities/coordenada";
+import { MarsRover } from "../entities/marsRover/marsRover";
+
+describe('Testing Comando', () => {
+
+    it('se Comando for "L" deve chamar GiraParaEsquerda', () => {
+        const coordenadaMock = new Coordenada(0,0);
+        const marsRoverMock = new MarsRover(coordenadaMock, Direcao.Norte);
+        const spy = jest.spyOn(marsRoverMock, "giraParaEsquerda");
+        const sut = comando("L", marsRoverMock);
+        expect(spy).toBeCalledTimes(1);
+    });
+
+    it('se Comando for "R" deve chamar GiraParaDireita', () => {
+        const coordenadaMock = new Coordenada(0,0);
+        const marsRoverMock = new MarsRover(coordenadaMock, Direcao.Norte);
+        const spy = jest.spyOn(marsRoverMock, "giraParaDireita");
+        const sut = comando("R", marsRoverMock);
+        expect(spy).toBeCalledTimes(1);
+    });
+
+    it('se Comando for "M" deve chamar "move"', () => {
+        const coordenadaMock = new Coordenada(0,0);
+        const marsRoverMock = new MarsRover(coordenadaMock, Direcao.Norte);
+        const spy = jest.spyOn(marsRoverMock, "move");
+        const sut = comando("M", marsRoverMock);
+        expect(spy).toBeCalledTimes(1);
+    });
+});
